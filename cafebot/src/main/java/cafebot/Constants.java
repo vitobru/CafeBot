c4b265fc2596d196b43ac24df1ab8c7215a6dd4f
Vito <jakobwilczynski211@gmail.com>
2021-04-19 17:17:40 -0700

    Initial commit

---
+++ cafebot/src/main/java/cafebot/Constants.java
@@ -0,0 +1,9 @@
+package cafebot;
+
+import java.awt.Color;
+
+public class Constants {
+
+    public static final Color PING_COLOR = new Color(255, 229, 216);
+
+}
---
+++ cafebot/src/main/java/cafebot/Main.java
@@ -0,0 +1,51 @@
+package cafebot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    static String prefix = "$";
+    public static void main(String[] args) throws IOException{
+
+        if (args.length != 1) {
+            System.err.println("This bot requires exactly one argument, "
+                    + "either a file with the token as content or the token directly.\n"
+                    + "If the argument is a relative file path, it is relative to the working directory");
+            System.exit(1);
+        }
+
+        DiscordApiBuilder apiBuilder = new DiscordApiBuilder();
+
+        // Token
+        Path tokenFile = Paths.get(args[0]);
+        if (Files.isRegularFile(tokenFile)) {
+            try (BufferedReader tokenFileReader = Files.newBufferedReader(tokenFile)) {
+                apiBuilder.setToken(tokenFileReader.readLine());
+            }
+        } else {
+            apiBuilder.setToken(args[0]);
+        }
+
+        // Login
+        DiscordApi api = apiBuilder
+                .setWaitForServersOnStartup(false)
+                .login().join();
+
+        // Listeners (Cogs)
+        api.addListener(new Ping());
+        /*
+        api.addListener(new NSFW());
+        api.addListener(new Administration());
+        api.addListener(new Music());
+        */ 
+
+    }
+
+}
